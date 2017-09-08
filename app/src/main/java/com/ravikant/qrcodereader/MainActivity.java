2bf556d4b232534f9fd320e6c8549fa89f1e5dc6
Ravikant Verma <ravikant@blazeautomation.com>
2017-09-08 14:31:07 +0530

    initial commit

---
+++ app/src/main/java/com/ravikant/qrcodereader/MainActivity.java
@@ -0,0 +1,107 @@
+package com.ravikant.qrcodereader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private TextView textView;
+    private FrameLayout frameLayout;
+    private IntentIntegrator qrScan;
+    private ZXingScannerView mScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        textView = (TextView) findViewById(R.id.textview);
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        frameLayout.setVisibility(View.GONE);
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        Button btn = (Button) findViewById(R.id.btnScan);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // qrScan.initiateScan();
+                QrScanner(v);
+            }
+        });
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    // textViewName.setText(obj.getString("name"));
+                    textView.setText(obj.getString("address"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mScannerView.stopCamera();
+    }
+
+    public void QrScanner(View view){
+        mScannerView = null;
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view<br />
+        frameLayout.addView(mScannerView);
+        frameLayout.setVisibility(View.VISIBLE);
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.<br />
+        mScannerView.startCamera();         // Start camera<br />
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        frameLayout.setVisibility(View.GONE);
+        mScannerView.stopCamera();
+        textView.setText(result.getText());
+        /*AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(result.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();*/
+    }
+}
